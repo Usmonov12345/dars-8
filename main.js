c3b632cccc9dd96edb222a8c09623374fd37691c
Usmonov12345 <toxirusmonov04@gmail.com>
2022-09-26 16:10:32 +0500

    8-dars

---
+++ main.js
@@ -0,0 +1,70 @@
+// const js = {
+//     document
+// }
+
+// named function expression
+
+// function getName (name){
+//     return name
+// }
+// // arrow function expression
+
+// const getAge = function(name){
+//     return name
+// }
+
+// const getEmail = (email) =>{
+//     return email
+// }
+
+// const powerUp = x => x **x;
+
+
+
+
+// const companies= [
+//     {name: "Company One", category: "Finance", start: 1981, end: 2004},
+//     {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+//     {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+//     {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+//     {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+//     {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+//     {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+//     {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+//     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+//   ];
+
+//   const ages = [33,44,55,5,77,11,22,34,43,2,34,54,3,2,22,2,3,4,44,3];
+
+
+//   for (let i =0; i<AbstractRange.length; i++){
+//     if(ages[i] > 20){
+//         console.log(ages[i]);
+//     }
+//   }
+
+
+
+//   const financeCompanies  = companies.forEach(function(company){
+
+//   })
+
+// let btn = document.querySelector("#btn")
+
+// let newComp =[]
+
+// btn.addEventListener("click", function(){
+//     companies.forEach(function(item){
+//         if(item.category === "Finance"){
+//             newComp.push(item)
+//         }
+//     })
+//     console.log(newComp);
+                            
+// })
+
+
+// function pushNewComp(companies){
+//     companies.forEach
+// }
+  
